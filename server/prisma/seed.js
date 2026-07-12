93abd7b4c353572fbb3056676caa11c72983f114
Pranay Narain <pranaynarain05@gmail.com>
2026-07-12 15:49:45 +0530

    new changes made

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -28,8 +28,8 @@ async function main() {
 main()
   .catch((e) => {
     console.error(e);
-    process.exit(1);
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   });
